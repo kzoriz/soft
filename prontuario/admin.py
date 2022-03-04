de17309487df05ae9f6fc7db2ff221c256fc94af
kzoriz <borisoliveira@alu.uern.br>
2022-03-04 20:55:17 -0300

    first commit

--- prontuario/admin.py
+++ prontuario/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
 
 # Register your models here.
-from prontuario.models import Prontuario, DadosMedicos, Procedimento
-
-admin.site.register(Prontuario)
-admin.site.register(DadosMedicos)
-admin.site.register(Procedimento)
