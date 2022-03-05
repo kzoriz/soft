0168a0828da546fcb036df27e1b94845d8bb2967
kzoriz <borisoliveira@alu.uern.br>
2022-03-04 21:48:50 -0300

    tudo ok!

--- distribuicao/views.py
+++ distribuicao/views.py
@@ -9,19 +9,19 @@ from django.views.generic import ListView
 from .models import Distribuicao, Disciplina
 # Create your views here.
 def distribuir_paciente_index(request):
-    return render(request, "distribuir_paciente/distribuir_paciente_index.html" )
+    return render(request, "distribuicao/distribuir_paciente_index.html" )
 
 
 class DistribuicaoCreateView(CreateView):
     model = Distribuicao
     fields = "__all__"
-    template_name = "distribuir_paciente/distribuicao.html"
+    template_name = "distribuicao/distribuicao.html"
     success_url = reverse_lazy("distribuir_pacientes_menu")
 
 class DisciplinaCreateView(CreateView):
     model = Disciplina
     fields = "__all__"
-    template_name = "distribuir_paciente/cadastro_disciplina.html"
+    template_name = "distribuicao/cadastro_disciplina.html"
     success_url = reverse_lazy("opcoes")
 
 def distribuicoes(request):
@@ -32,7 +32,7 @@ def distribuicoes(request):
         "todas_distribuicoes": todas_distribuicoes,
 
     }
-    return render(request, "distribuir_paciente/distribuicoes.html", context)
+    return render(request, "distribuicao/distribuicoes.html", context)
 
 def disciplinas(request):
     todas_disciplinas = Disciplina.objects.all()
@@ -42,24 +42,24 @@ def disciplinas(request):
         "todas_disciplinas": todas_disciplinas,
 
     }
-    return render(request, "distribuir_paciente/disciplinas.html", context)
+    return render(request, "distribuicao/disciplinas.html", context)
 
 def disciplina_detalhes(request, pk = None):
     disciplina = Disciplina.objects.get(pk=pk)
     context = {
         'disciplina': disciplina
     }
-    return render(request, 'distribuir_paciente/disciplina_detalhes.html', context)
+    return render(request, 'distribuicao/disciplina_detalhes.html', context)
 
 class DisciplinaUpdate(LoginRequiredMixin, UpdateView):
     login_url = reverse_lazy("login")
     model = Disciplina
     fields = "__all__"
-    template_name = "distribuir_paciente/disciplina_editar.html"
+    template_name = "distribuicao/disciplina_editar.html"
     success_url = reverse_lazy("disciplinas")
 
 class DisciplinaDelete(LoginRequiredMixin,DeleteView):
     login_url = reverse_lazy("login")
     model = Disciplina
-    template_name = "distribuir_paciente/disciplina_delete.html"
+    template_name = "distribuicao/disciplina_delete.html"
     success_url = reverse_lazy("disciplinas")
