bf8e116a5b0237db12417cec7c7509eff3aa1497
kzoriz <borisoliveira@alu.uern.br>
2022-03-06 00:50:24 -0300

    deploy heroku

--- distribuicao/models.py
+++ distribuicao/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-from pacientes.models import Paciente
-from usuarios.models import Discente, Docente
-
+from usuarios.models import Docente
 
 
 class Disciplina(models.Model):
@@ -18,33 +16,11 @@ class Disciplina(models.Model):
         ('10º', '10º'),
 
     ]
+
     nome = models.CharField(verbose_name="Nome Disciplina", max_length=200)
     codigo = models.CharField(verbose_name="CodDisc", max_length=200)
     periodo = models.CharField(verbose_name="Periodo", max_length=3, choices=PERIODO)
-    matricula_docente = models.ForeignKey(Docente, verbose_name="MatDocente", on_delete=models.SET_NULL, null=True)
-
 
     def __str__(self):
         return self.nome
-class Distribuicao(models.Model):
-
-    PROCEDIMENTO = [
-        ('restaurações', 'RESTAURAÇÃO'),
-        ('aplicação de flúor', 'APLICAÇÃO FLUOR'),
-        ('tratamentos de canal', 'TRATAMENTO CANAL'),
-        ('tratamento de bruxismo', 'TRATAMENTO BRUXISMO'),
-
-    ]
-
-    paciente = models.OneToOneField(Paciente, on_delete=models.CASCADE, null=True)
-    disciplina = models.ForeignKey(Disciplina, on_delete=models.SET_NULL, null=True)
-    discente = models.ForeignKey(Discente, on_delete=models.SET_NULL, null=True)
-    docente = models.ForeignKey(Docente, on_delete=models.SET_NULL, null=True)
-    data_atendimento = models.DateTimeField(verbose_name="Data/Hora Atendimento")
-    procedimento = models.CharField(verbose_name="Procedimento", max_length=200, choices=PROCEDIMENTO)
-
-
-    def __str__(self):
-        return self.procedimento
-
 
