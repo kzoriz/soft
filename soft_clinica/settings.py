35195dc47fd9636d14bc3a8296c9905f740b1fee
kzoriz <borisoliveira@alu.uern.br>
2022-03-05 22:18:16 -0300

    deploy heroku

--- soft_clinica/settings.py
+++ soft_clinica/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
 
     #my apps
     'pacientes.apps.PacientesConfig',
-    'usuarios.apps.UsuariosConfig',
     'distribuicao.apps.DistribuicaoConfig',
     'prontuario.apps.ProntuarioConfig',
 
