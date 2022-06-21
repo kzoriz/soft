85701d8c29cf0c70a8192cd1e59f81b59e79716c
kzoriz <borisoliveira@alu.uern.br>
2022-06-21 00:30:38 -0300

    make it better

--- prontuario/views.py
+++ prontuario/views.py
@@ -1,4 +1,4 @@
-
+#fdfdf
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render, redirect, get_object_or_404
